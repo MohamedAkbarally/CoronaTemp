cd78195f9b733c75e0a983f21730b7982df90a27
Mohamed Akbarally <mohamed.akbarally@gmail.com>
2020-04-01 04:45:21 +1100

    until march 30th

--- Cases_Plot_All.py
+++ Cases_Plot_All.py
@@ -6,16 +6,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data_df = pd.read_csv('data_cases.csv')
+data_df = pd.read_csv('./Data/data_cases.csv')
 
 # Store of all temperatures that caused a case
 temps = []
+countries = []
+locations = []
+humidities = []
+dewpoints = []
+temperatureHighs = []
+uvIndexes = []
+
 
 for index, row in data_df.iterrows():
     # Start date of recorded data
     d = datetime.date(2020, 1, 22)
 
-    # Location (used only for debugging)
+    # Location
     location = row["Country/Region"]
     if not pd.isnull(row["Province/State"]):
         location += " " + row["Province/State"]
@@ -28,7 +35,7 @@ for index, row in data_df.iterrows():
     casecount_total = 0
 
     # iterates from Jan 22nd to March 22nd
-    for i in range(0, 60):
+    for i in range(0, 67):
 
         strdate = str(d.month) + "/" + str(d.day) + "/" + "20"
 
@@ -54,6 +61,22 @@ for index, row in data_df.iterrows():
         # Adds temperature to temps array for every case
         temps.extend([data_weather["daily"]["data"][0]
                       ["temperatureLow"]]*new_cases)
+        # other potential variables of interest
+        humidities.extend([data_weather["daily"]["data"][0]
+                           ["humidity"]]*new_cases)
+
+        dewpoints.extend([data_weather["daily"]["data"][0]
+                          ["dewPoint"]]*new_cases)
+
+        temperatureHighs.extend([data_weather["daily"]["data"][0]
+                                 ["temperatureHigh"]]*new_cases)
+
+        uvIndexes.extend([data_weather["daily"]["data"][0]
+                          ["uvIndex"]]*new_cases)
+
+        # Stores Location
+        countries.extend([row["Country/Region"]]*new_cases)
+        locations.extend([location]*new_cases)
 
         # Sets the total cases new case count
         casecount_total = casecount
@@ -62,72 +85,117 @@ for index, row in data_df.iterrows():
         d = d+datetime.timedelta(days=1)
 
 
-# Since data from March 22nd onwards is stored in a different format
-# The following function calculates the cases from March 23nd to March 29th
-d = datetime.date(2020, 3, 22)
+data_df = pd.read_csv('./Data/data_cases_us.csv')
 
-for i in range(0, 7):
-    # Reads data for current day
-    srtdate = d.strftime("%m-%d-%Y")
-    today_df = pd.read_csv(
-        "data_daily/"+srtdate+".csv")
-    today_df = today_df[["Confirmed", "Combined_Key", "Lat", "Long_"]]
+for index, row in data_df.iterrows():
+    # Start date of recorded data
+    d = datetime.date(2020, 1, 22)
 
-    # Elimates data without coordinates (e.g. Cruise Ships)
-    today_df = today_df[today_df.Lat != 0]
+    if row["Admin2"] == "Unassigned":
+        continue
 
-    d = d+datetime.timedelta(days=1)
+    # Location
+    location = row["Country_Region"] + " " + row["Province_State"]
 
-    # Stores data for the next day
-    srtdate = d.strftime("%m-%d-%Y")
-    tommorow_df = pd.read_csv(
-        "data_daily/"+srtdate+".csv")
-    tommorow_df = tommorow_df[["Confirmed", "Combined_Key", "Lat", "Long_"]]
-    tommorow_df = tommorow_df[tommorow_df.Lat != 0]
+    print(location)
 
-    # Combines dataframes
-    merge_df = tommorow_df.merge(today_df, on='Combined_Key',
-                                 how='left', suffixes=('_1', '_2'))
+    # Coordinates at location
+    cords = str(row["Lat"]) + \
+        ","+str(row["Long_"])
 
-    # Clean Up
-    merge_df = merge_df.fillna(0)
-    merge_df = merge_df[merge_df.Lat_1 != 0]
-    merge_df = merge_df[merge_df.Lat_2 != 0]
+    casecount_total = 0
 
-    # Calculates change in cases
-    merge_df["Confirmed"] = merge_df["Confirmed_1"] - merge_df["Confirmed_2"]
+    # iterates from Jan 22nd to March 22nd
+    for i in range(0, 67):
 
-    # Iterates through every location
-    for index, row in merge_df.iterrows():
+        strdate = str(d.month) + "/" + str(d.day) + "/" + "20"
 
-        cords = str(row["Lat_1"]) + \
-            ","+str(row["Long__1"])
-
-        # Finds the date 5 days prior to the infection
-        d_infection = d-datetime.timedelta(days=5)
-        unixtime = time.mktime(d_infection.timetuple())
-
-        # Skips if there hasnt been a case
-        if int(row["Confirmed"]) == 0:
+        if strdate == "3/22/20":
             continue
 
-        # Finds the temperature low at cords on d_infection
+        # gets casecount on date
+        casecount = row[strdate]
+
+        new_cases = casecount-casecount_total
+
+        # checks if there was an increase in cases
+        if (new_cases) < 1:
+            d = d+datetime.timedelta(days=1)
+            continue
+
+        # finds the date of infection
+        d_infection = d-datetime.timedelta(days=5)
+
+        # finds the temperature low at the given coordinates on the date of infection
+        unixtime = time.mktime(d_infection.timetuple())
         response_weather = requests.get(
             "https://api.darksky.net/forecast/e2250d5bc97d1b8e39223aa95235d995/"+cords+","+str(int(unixtime))+"?exclude=currently,flags")
         data_weather = response_weather.json()
+
+        # Adds temperature to temps array for every case
         temps.extend([data_weather["daily"]["data"][0]
-                      ["temperatureLow"]]*int(row["Confirmed"]))
+                      ["temperatureLow"]]*new_cases)
+
+        # other potential variables of interest
+        humidities.extend([data_weather["daily"]["data"][0]
+                           ["humidity"]]*new_cases)
+
+        dewpoints.extend([data_weather["daily"]["data"][0]
+                          ["dewPoint"]]*new_cases)
+
+        temperatureHighs.extend([data_weather["daily"]["data"][0]
+                                 ["temperatureHigh"]]*new_cases)
+
+        uvIndexes.extend([data_weather["daily"]["data"][0]
+                          ["uvIndex"]]*new_cases)
+
+        # Stores Location
+        countries.extend([row["Country_Region"]]*new_cases)
+        locations.extend([location]*new_cases)
+
+        # Sets the total cases new case count
+        casecount_total = casecount
+
+        # increments date
+        d = d+datetime.timedelta(days=1)
 
 
 # Converts Temperature to Celcius
 temps = [x - 32 for x in temps]
 temps = [x * (5/9) for x in temps]
 
+temperatureHighs = [x - 32 for x in temperatureHighs]
+temperatureHighs = [x * (5/9) for x in temperatureHighs]
+
+tempsavg = [(a+b)/2 for a, b in zip(temperatureHighs, temps)]
+
+# csv export
+csv = {'Country': countries,
+       'Temperature Low': temps,
+       'Country & Province': locations,
+       'Humidity': humidities,
+       'Dew Point': dewpoints,
+       'Temperature High': temperatureHighs,
+       'Temperature Avg': tempsavg,
+       'Uv Index': uvIndexes
+       }
+
+df = pd.DataFrame(
+    csv, columns=['Country', 'Temperature Low', 'Country & Province', 'Humidity', 'Dew Point', 'Temperature High', 'Uv Index'])
+
+df.to_csv('./Output/cases_all.csv', index=False, header=True)
+
 # Plots Graph
-plt.hist(temps, bins=np.arange(-20, 30, 2))
-plt.xticks(np.arange(-20, 30, 2))
+plt.rcParams['axes.axisbelow'] = True
+plt.hist(temps, bins=np.arange(-20, 30, 2), edgecolor="black",
+         linewidth='1.5', facecolor='b', alpha=0.9)
+plt.xticks(np.arange(-20, 30, 4))
 plt.title('Effect of Temperature on Cases due to Corona Virus')
-plt.xlabel('Temperature 5 days before case')
+plt.xlabel('Temperature Low 5 days before case')
 plt.ylabel('Cases')
+plt.grid(linestyle=':')
+plt.savefig('./Output/Case_Plot.png', bbox_inches='tight')
 
 plt.show()
+
+print(df)
--- Deaths_Plot_All.py
+++ Deaths_Plot_All.py
@@ -6,16 +6,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data_df = pd.read_csv('data_deaths.csv')
+data_df = pd.read_csv('./Data/data_deaths.csv')
 
 # Store of all temperatures that caused a death
 temps = []
+countries = []
+locations = []
+humidities = []
+dewpoints = []
+temperatureHighs = []
+uvIndexes = []
+
 
 for index, row in data_df.iterrows():
     # Start date of recorded data
     d = datetime.date(2020, 1, 22)
 
-    # Location (used only for debugging)
+    # Location
     location = row["Country/Region"]
     if not pd.isnull(row["Province/State"]):
         location += " " + row["Province/State"]
@@ -28,7 +35,7 @@ for index, row in data_df.iterrows():
     deathcount_total = 0
 
     # iterates from Jan 22nd to March 22nd
-    for i in range(0, 60):
+    for i in range(0, 67):
 
         strdate = str(d.month) + "/" + str(d.day) + "/" + "20"
 
@@ -54,6 +61,22 @@ for index, row in data_df.iterrows():
         # Adds temperature to temps array for every death
         temps.extend([data_weather["daily"]["data"][0]
                       ["temperatureLow"]]*new_deaths)
+        # other potential variables of interest
+        humidities.extend([data_weather["daily"]["data"][0]
+                           ["humidity"]]*new_deaths)
+
+        dewpoints.extend([data_weather["daily"]["data"][0]
+                          ["dewPoint"]]*new_deaths)
+
+        temperatureHighs.extend([data_weather["daily"]["data"][0]
+                                 ["temperatureHigh"]]*new_deaths)
+
+        uvIndexes.extend([data_weather["daily"]["data"][0]
+                          ["uvIndex"]]*new_deaths)
+
+        # Stores Location
+        countries.extend([row["Country/Region"]]*new_deaths)
+        locations.extend([location]*new_deaths)
 
         # Sets the total deaths new death count
         deathcount_total = deathcount
@@ -61,72 +84,117 @@ for index, row in data_df.iterrows():
         # increments date
         d = d+datetime.timedelta(days=1)
 
-# Since data from March 22nd onwards is stored in a different format
-# The following function calculates the deaths from March 23nd to March 29th
-d = datetime.date(2020, 3, 22)
 
-for i in range(0, 7):
-    # Reads data for current day
-    srtdate = d.strftime("%m-%d-%Y")
-    today_df = pd.read_csv(
-        "data_daily/"+srtdate+".csv")
-    today_df = today_df[["Deaths", "Combined_Key", "Lat", "Long_"]]
+data_df = pd.read_csv('./Data/data_deaths_us.csv')
 
-    # Elimates data without coordinates (e.g. Cruise Ships)
-    today_df = today_df[today_df.Lat != 0]
+for index, row in data_df.iterrows():
+    # Start date of recorded data
+    d = datetime.date(2020, 1, 22)
 
-    d = d+datetime.timedelta(days=1)
+    if row["Admin2"] == "Unassigned":
+        continue
 
-    # Stores data for the next day
-    srtdate = d.strftime("%m-%d-%Y")
-    tommorow_df = pd.read_csv(
-        "data_daily/"+srtdate+".csv")
-    tommorow_df = tommorow_df[["Deaths", "Combined_Key", "Lat", "Long_"]]
-    tommorow_df = tommorow_df[tommorow_df.Lat != 0]
+    # Location
+    location = row["Country_Region"] + " " + row["Province_State"]
 
-    # Combines dataframes
-    merge_df = tommorow_df.merge(today_df, on='Combined_Key',
-                                 how='left', suffixes=('_1', '_2'))
+    print(location)
 
-    # Clean Up
-    merge_df = merge_df.fillna(0)
-    merge_df = merge_df[merge_df.Lat_1 != 0]
-    merge_df = merge_df[merge_df.Lat_2 != 0]
+    # Coordinates at location
+    cords = str(row["Lat"]) + \
+        ","+str(row["Long_"])
 
-    # Calculates change in deaths
-    merge_df["Deaths"] = merge_df["Deaths_1"] - merge_df["Deaths_2"]
+    deathcount_total = 0
 
-    # Iterates through every location
-    for index, row in merge_df.iterrows():
+    # iterates from Jan 22nd to March 22nd
+    for i in range(0, 67):
 
-        cords = str(row["Lat_1"]) + \
-            ","+str(row["Long__1"])
+        strdate = str(d.month) + "/" + str(d.day) + "/" + "20"
 
-        # Finds the date 15 days prior to the infection
-        d_infection = d-datetime.timedelta(days=15)
-        unixtime = time.mktime(d_infection.timetuple())
-
-        # Skips if there hasnt been a death
-        if int(row["Deaths"]) == 0:
+        if strdate == "3/22/20":
             continue
 
-        # Finds the temperature low at cords on d_infection
+        # gets deathcount on date
+        deathcount = row[strdate]
+
+        new_deaths = deathcount-deathcount_total
+
+        # checks if there was an increase in deaths
+        if (new_deaths) < 1:
+            d = d+datetime.timedelta(days=1)
+            continue
+
+        # finds the date of infection
+        d_infection = d-datetime.timedelta(days=15)
+
+        # finds the temperature low at the given coordinates on the date of infection
+        unixtime = time.mktime(d_infection.timetuple())
         response_weather = requests.get(
             "https://api.darksky.net/forecast/e2250d5bc97d1b8e39223aa95235d995/"+cords+","+str(int(unixtime))+"?exclude=currently,flags")
         data_weather = response_weather.json()
+
+        # Adds temperature to temps array for every death
         temps.extend([data_weather["daily"]["data"][0]
-                      ["temperatureLow"]]*int(row["Deaths"]))
+                      ["temperatureLow"]]*new_deaths)
+
+        # other potential variables of interest
+        humidities.extend([data_weather["daily"]["data"][0]
+                           ["humidity"]]*new_deaths)
+
+        dewpoints.extend([data_weather["daily"]["data"][0]
+                          ["dewPoint"]]*new_deaths)
+
+        temperatureHighs.extend([data_weather["daily"]["data"][0]
+                                 ["temperatureHigh"]]*new_deaths)
+
+        uvIndexes.extend([data_weather["daily"]["data"][0]
+                          ["uvIndex"]]*new_deaths)
+
+        # Stores Location
+        countries.extend([row["Country_Region"]]*new_deaths)
+        locations.extend([location]*new_deaths)
+
+        # Sets the total deaths new death count
+        deathcount_total = deathcount
+
+        # increments date
+        d = d+datetime.timedelta(days=1)
 
 
 # Converts Temperature to Celcius
 temps = [x - 32 for x in temps]
 temps = [x * (5/9) for x in temps]
 
-# Plots Graph
-plt.hist(temps, bins=np.arange(-20, 30, 2))
-plt.xticks(np.arange(-20, 30, 2))
+temperatureHighs = [x - 32 for x in temperatureHighs]
+temperatureHighs = [x * (5/9) for x in temperatureHighs]
+
+tempsavg = [(a+b)/2 for a, b in zip(temperatureHighs, temps)]
+
+# csv export
+csv = {'Country': countries,
+       'Temperature Low': temps,
+       'Country & Province': locations,
+       'Humidity': humidities,
+       'Dew Point': dewpoints,
+       'Temperature High': temperatureHighs,
+       'Temperature Avg': tempsavg,
+       'Uv Index': uvIndexes
+       }
+
+df = pd.DataFrame(
+    csv, columns=['Country', 'Temperature Low', 'Country & Province', 'Humidity', 'Dew Point', 'Temperature High', 'Uv Index'])
+
+df.to_csv('./Output/deaths_all.csv', index=False, header=True)
+
+plt.rcParams['axes.axisbelow'] = True
+plt.hist(temps, bins=np.arange(-20, 30, 2), edgecolor="black",
+         linewidth='1.5', facecolor='red', alpha=0.9)
+plt.xticks(np.arange(-20, 30, 4))
 plt.title('Effect of Temperature on Deaths due to Corona Virus')
-plt.xlabel('Temperature 15 days before death')
+plt.xlabel('Temperature Low 15 days before death')
 plt.ylabel('Deaths')
+plt.grid(linestyle=':')
+plt.savefig('./Output/Death_Plot.png'', bbox_inches='tight')
 
 plt.show()
+
+print(df)
